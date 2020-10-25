bc981084f98855fa41a6b193c4374b7d7fe541af
Etra-Games <projectetraflow@gmail.com>
2020-10-25 18:24:44 -0500

    Luke's Test Commit Scene/Project
    I'm commiting this to a seperate branch, going to research pull requests now.

---
+++ Assets/Luke's Test Project/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 locationToMoveTo = new Vector3(Input.GetAxisRaw("P1KeyboardHorizontal"), 0, Input.GetAxisRaw("P1KeyboardVertical"));
+        transform.Translate(locationToMoveTo * speed * Time.deltaTime);
+    }
+}
